8651a309f4c1c9ebe1903d5ff416aea10776688b
witoldbrzezinski <witbrzezinski@gmail.com>
2023-09-27 20:11:41 +0200

    first commit

---
+++ src/test/java/pl/witoldbrzezinski/currencies/CurrenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.witoldbrzezinski.currencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
